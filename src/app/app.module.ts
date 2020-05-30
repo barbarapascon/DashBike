020d36e3c9eed11e8e63b68657720cda9c235ed1
Barbara Pascon de Azevedo Marques <barbarapascon@MacBook-Air-de-Barbara.local>
2020-05-29 21:13:08 -0300

    adiciona sidebar, e menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
+    DefaultModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
