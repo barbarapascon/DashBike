8015ddb6950d50d2e6116518171a4b3da80f5d95
Barbara Pascon de Azevedo Marques <barbarapascon@MacBook-Air-de-Barbara.local>
2020-05-30 14:40:16 -0300

    faz layout basico das paginas de autenticacao

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,15 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ResetPasswordComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ],
   exports:[
     LoginComponent,
