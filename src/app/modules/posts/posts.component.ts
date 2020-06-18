98c3a6f1505bdf09e5ec6f9c4ebbda87bd8a2512
Barbara Pascon de Azevedo Marques <barbarapascon@hotmail.com>
2020-06-18 03:08:01 -0300

    cria Bike service e traz todas bikes

--- src/app/modules/posts/posts.component.ts
+++ src/app/modules/posts/posts.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatAccordion } from '@angular/material';
+import { BikeService } from 'src/app/shared/services/bike.service';
+import { Bike } from 'src/app/shared/models/bike';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-posts',
@@ -7,10 +10,21 @@ import { MatAccordion } from '@angular/material';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
+  currentUser: User;
+  bike = {} as Bike;
+  bikes: Bike[];
+  
   @ViewChild(MatAccordion) accordion: MatAccordion;
-  constructor() { }
+  constructor(private bikeService: BikeService) { }
 
   ngOnInit(): void {
+    this.currentUser = JSON.parse(localStorage.getItem('user'))
+    this.getBikes();
+  }
+  getBikes() {
+    this.bikeService.getBikes(this.currentUser.token).subscribe((bikes: Bike[]) => {
+      this.bikes = bikes;
+    });
   }
 
 }
